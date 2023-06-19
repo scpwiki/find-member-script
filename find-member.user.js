deb5d1a1e77d15e62e654069df15b6d739047cab
Emmie Maeda <emmie.maeda@gmail.com>
2023-06-18 20:48:34 -0400

    Add download URL.

--- find-member.user.js
+++ find-member.user.js
@@ -8,8 +8,8 @@
 // @name        Wikidot find member script
 // @description Adds a button to search for members in the Wikidot admin panel.
 // @version     v0.0.1
-// @updateURL
-// @downloadURL
+// @updateURL   https://github.com/scpwiki/find-member-script/raw/main/find-member.user.js
+// @downloadURL https://github.com/scpwiki/find-member-script/raw/main/find-member.user.js
 // @supportURL  https://www.wikidot.com/account/messages#/new/4598089
 // @include     https://*.wikidot.com/_admin
 // ==/UserScript==
