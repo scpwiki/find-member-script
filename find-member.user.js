cd91fa36f3a7bce8aae2419f7f15e96b1f3825df
Emmie Maeda <emmie.maeda@gmail.com>
2023-06-18 23:06:01 -0400

    Bump version to v0.1.0

--- find-member.user.js
+++ find-member.user.js
@@ -7,7 +7,7 @@
 // ==UserScript==
 // @name        Wikidot find member script
 // @description Adds a button to search for members in the Wikidot admin panel.
-// @version     v0.0.1
+// @version     v0.1.0
 // @updateURL   https://github.com/scpwiki/find-member-script/raw/main/find-member.user.js
 // @downloadURL https://github.com/scpwiki/find-member-script/raw/main/find-member.user.js
 // @supportURL  https://www.wikidot.com/account/messages#/new/4598089
