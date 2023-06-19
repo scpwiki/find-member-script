b361f785452800c6638346a104c97188ec477f9c
Emmie Maeda <emmie.maeda@gmail.com>
2023-06-18 20:47:43 -0400

    Create userscript stub.

---
+++ find-member.user.js
@@ -0,0 +1,17 @@
+/*
+ * Wikidot member list locator userscript
+ *
+ * For installation instructions, see https://scpwiki.com/usertools
+ */
+
+// ==UserScript==
+// @name        Wikidot find member script
+// @description Adds a button to search for members in the Wikidot admin panel.
+// @version     v0.0.1
+// @updateURL
+// @downloadURL
+// @supportURL  https://www.wikidot.com/account/messages#/new/4598089
+// @include     https://*.wikidot.com/_admin
+// ==/UserScript==
+
+// TODO
