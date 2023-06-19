c9e1bc094db58aef769a0bc0eddad1f1b82a91d8
Emmie Maeda <emmie.maeda@gmail.com>
2023-06-18 21:00:09 -0400

    Add UI elements for startup.

--- find-member.user.js
+++ find-member.user.js
@@ -14,4 +14,28 @@
 // @include     https://*.wikidot.com/_admin
 // ==/UserScript==
 
-// TODO
+function runBinarySearch(dateEntryInput) {
+  // TODO
+}
+
+function main() {
+  // Create the UI elements
+  const searchContainer = document.createElement('div');
+  const dateEntryInput = document.createElement('input');
+  dateEntryInput.setAttribute('type', 'text');
+  dateEntryInput.setAttribute('size', '10');
+  dateEntryInput.setAttribute('placeholder', '12 May 2014');
+  searchContainer.appendChild(dateEntryInput);
+
+  const searchButton = document.createElement('button');
+  searchButton.innerText = 'Search';
+  searchButton.classList.add('btn', 'btn-xs')
+  searchButton.title = 'Search all member pages until the page with this date on it is found';
+  searchButton.addEventListener('click', () => runBinarySearch(dateEntryInput));
+  searchContainer.appendChild(searchButton);
+
+  const membersTab = document.querySelector('#MembersTab');
+  membersTab.appendChild(searchContainer);
+}
+
+main();
